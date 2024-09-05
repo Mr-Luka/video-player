693638d772316830e500478126db5c548b7f3fcf
Luka <luka.luka.luka93@gmail.com>
2024-09-05 11:34:17 +0200

    play and pause logo changes when pressed

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ const stop = document.querySelector('#stop');
 const progress = document.querySelector('#progress');
 const timestamp = document.querySelector('#timestamp')
 
-play.addEventListener('click', ()=> video.play());
-stop.addEventListener('click', ()=> video.pause());
-progress.addEventListener('input', ()=> video.currentTime = progress.value);
+
+function playPause() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateIcon() {
+    if (video.paused) {
+        play.innerHTML = `<i class="fa fa-play fa-2x"></i>`;
+    } else {
+        play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`
+    }
+}
+// to play when I click on a video or play button
+video.addEventListener('click', playPause);
+play.addEventListener('click', playPause);
+video.addEventListener('pause', updateIcon);
+video.addEventListener('play', updateIcon);
+
