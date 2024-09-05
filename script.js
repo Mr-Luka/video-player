8e140206acad1676fd46885dc5b8ee5b117987e3
Luka <luka.luka.luka93@gmail.com>
2024-09-05 11:44:49 +0200

    function that will show minutes and seconds

--- script.js
+++ script.js
@@ -20,9 +20,33 @@ function updateIcon() {
         play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`
     }
 }
+
+function stopVideo(){
+    video.currentTime = 0;
+    video.pause();
+}
+
+function updateProgress () {
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    // Get minutes
+    let minutes = Math.floor(video.currentTime / 60);
+    if (minutes < 10){
+        minutes = '0' + String(minutes);
+    }
+
+    let seconds = Math.floor(video.currentTime % 60);
+    if (seconds < 10) {
+        seconds = '0' + String(seconds);
+    }
+    timestamp.innerHTML = `${minutes}:${seconds}`
+}
 // to play when I click on a video or play button
 video.addEventListener('click', playPause);
-play.addEventListener('click', playPause);
 video.addEventListener('pause', updateIcon);
 video.addEventListener('play', updateIcon);
+video.addEventListener('timeupdate', updateProgress)
+
+play.addEventListener('click', playPause);
+stop.addEventListener('click', stopVideo);
 
